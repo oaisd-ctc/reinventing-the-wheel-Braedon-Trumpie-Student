6aa069b0245125820b6c6f1d1c05e9e9616312d0
Braedon-Trumpie-Student <112421997+Braedon-Trumpie-Student@users.noreply.github.com>
2023-01-24 19:41:58 +0000

    outlier detection done

--- Wheel/Program.cs
+++ Wheel/Program.cs
@@ -3,10 +3,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int[] array = {5, 7, 11, -6, 45, 100};
-        ArrayUtils.SortAscending(array);
-        foreach(int i in array)
-            Console.Write(i + " ");
-        Console.WriteLine();
+        int[] array = {5, 1, 11, -6, 45, 100};
+        double[] arr = {-500, 3.0, 4.0, 10.0, 11.0, 50.0, 60.0, 80.0, 90.0, 195};
+        
+        Console.WriteLine(Arrays.ArrayUtils.GetIQR(arr));
+        Console.WriteLine(Arrays.ArrayUtils.GetNumberOfOutliers(arr));
     }
 }
